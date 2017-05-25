5096d9d1bdd75a491c3b44478b208d713e3d69d8
AlexeyRakcheev <rakcheev77@yandex.ru>
2017-05-25 21:54:09 +0300

    Add files via upload

---
+++ lab.java
@@ -0,0 +1,10 @@
+public class lab
+{
+	public static void main(String [] arg) 
+	{ 
+		String in="";
+		char result;
+		result = in.charAt (3);
+		System.out.println(result);
+	}
+}		
---
+++ palindrome.java
@@ -0,0 +1,12 @@
+public class palindrome
+{
+	public static void main (String [] args)
+	{
+		String in="";
+		for (int count=0; count<args.length; count++)
+			{
+				in = args [count];
+			}
+		System.out.println("vvedite"+in);
+	}
+}	
